80b58a6c2ccd4b284b0b171200e1599b33c67553
NukuHack <109027819+NukuHack@users.noreply.github.com>
2024-10-16 23:22:55 +0200

    Update THE_machines.js
    ?

--- kubejs/server_scripts/big_mods/gtceu/new_machine_recipes/THE_machines.js
+++ kubejs/server_scripts/big_mods/gtceu/new_machine_recipes/THE_machines.js
@@ -15,7 +15,7 @@ ServerEvents.recipes(event => {
         .EUt(16);
 */
     greg.assembler('gfs:huge_chemical_reactor')
-        .itemInputs('gtceu:large_chemical_reactor',"6x #forge:plates/polyethylene")
+        .itemInputs('gtceu:large_chemical_reactor',"6x #forge:plates/polyethylene","6x #forge:plates/polytetrafluoroethylene")
         .itemOutputs('gtceu:huge_chemical_reactor')
         .circuit(23)
         .duration(20*80)

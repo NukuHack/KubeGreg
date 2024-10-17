91287f225626c9cc497ee7db85019dd910aaa53c
NukuHack <109027819+NukuHack@users.noreply.github.com>
2024-10-17 13:43:04 +0200

    Update cores.js
    ff mis-spelling

--- kubejs/server_scripts/big_mods/ae2/cores.js
+++ kubejs/server_scripts/big_mods/ae2/cores.js
@@ -7,14 +7,14 @@ ServerEvents.recipes(event => {
     greg.assembler("gfs:formation_core")
         .itemInputs("#gtceu:circuits/ulv","2x #forge:dusts/fluix",
 		"#forge:gems/certus_quartz", "ae2:logic_processor")
-        .inputFluids(Fuid.of("gtceu:red_alloy",288))
+        .inputFluids(Fluid.of("gtceu:red_alloy",288))
         .itemOutputs("ae2:formation_core")
         .EUt(480)
         .duration(100);
     greg.assembler("gfs:annihilation_core")
         .itemInputs("#gtceu:circuits/ulv","2x #forge:dusts/fluix",
 		"#forge:gems/quartz", "ae2:logic_processor")
-        .inputFluids(Fuid.of("gtceu:red_alloy",288))
+        .inputFluids(Fluid.of("gtceu:red_alloy",288))
         .itemOutputs("ae2:annihilation_core")
         .EUt(480)
         .duration(100);

73190d18fd0b37a6615d50ec69b9f975786093db
NukuHack <109027819+NukuHack@users.noreply.github.com>
2024-10-21 15:00:58 +0200

    Update inscriber.js
    stuupid (and some fixes)

--- kubejs/server_scripts/big_mods/ae2/inscriber.js
+++ kubejs/server_scripts/big_mods/ae2/inscriber.js
@@ -20,21 +20,21 @@ ServerEvents.recipes(event => {
             C: "gtceu:annealed_copper_plate"
         }
     );
-
+	
     function press_recipe(mod, name, color, ingredient) {
         greg.laser_engraver("gfs:" + name + "_press")
             .itemInputs("gtceu:stainless_steel_plate")
             .notConsumable("#forge:lenses/" + color)
             .itemOutputs(mod + name + "_press")
             .EUt(480)
-            .duration(1600);
+            .duration(600);
         greg.inscriber("gfs:" + name + "_print")
             .itemInputs("#forge:plates/" + ingredient)
             .notConsumable(mod + name + "_press")
             .itemOutputs(mod + "printed_" + name)
             .inputFluids(Fluid.of("minecraft:water", 500))
             .EUt(240)
-            .duration(240);
+            .duration(300);
         greg.inscriber("gfs:" + name + "_print_fast")
             .itemInputs("#forge:plates/" + ingredient)
             .notConsumable(mod + name + "_press")
@@ -48,19 +48,19 @@ ServerEvents.recipes(event => {
                 .inputFluids(Fluid.of("gtceu:tin", 576))
                 .itemOutputs(mod + name)
                 .EUt(240)
-                .duration(240);
+                .duration(200);
             greg.inscriber("gfs:" + name + "_solder")
                 .itemInputs(mod + "printed_" + name, "ae2:printed_silicon", "#gtceu:circuits/hv")
                 .inputFluids(Fluid.of("gtceu:soldering_alloy", 288))
                 .itemOutputs(mod + name)
                 .EUt(240)
-                .duration(200);
+                .duration(160);
             greg.inscriber("gfs:" + name + "_polybenzimidazole")
                 .itemInputs(mod + "printed_" + name, "ae2:printed_silicon", "#gtceu:circuits/hv")
                 .inputFluids(Fluid.of("gtceu:polybenzimidazole", 144))
                 .itemOutputs(mod + name)
                 .EUt(240)
-                .duration(140);
+                .duration(120);
         }
     }
     press_recipe("ae2:", "logic_processor", "yellow", "electrum");
@@ -68,4 +68,46 @@ ServerEvents.recipes(event => {
     press_recipe("ae2:", "engineering_processor", "blue", "diamond");
     press_recipe("megacells:", "accumulation_processor", "black", "black_steel");
     press_recipe("ae2:", "silicon", "gray", "silicon");
+	
+	
+	
+	event.shapeless("gfs:stupid_press",["ae2:logic_processor_press","ae2:calculation_processor_press",
+	"ae2:silicon_press","ae2:engineering_processor_press","megacells:accumulation_processor_press"]);
+	
+    greg.inscriber("gfs:stupid_print_logic")
+        .itemInputs("#forge:plates/electrum")
+        .notConsumable("gfs:stupid_press")
+        .itemOutputs("ae2:printed_logic_processor")
+        .inputFluids(Fluid.of("gtceu:lubricant", 500))
+        .EUt(240)
+        .duration(240);
+    greg.inscriber("gfs:stupid_print_calculation")
+        .itemInputs("#forge:plates/certus_quartz")
+        .notConsumable("gfs:stupid_press")
+        .itemOutputs("ae2:printed_calculation_processor")
+        .inputFluids(Fluid.of("gtceu:lubricant", 500))
+        .EUt(240)
+        .duration(240);
+    greg.inscriber("gfs:stupid_print_silicon")
+        .itemInputs("#forge:plates/silicon")
+        .notConsumable("gfs:stupid_press")
+        .itemOutputs("ae2:printed_silicon")
+        .inputFluids(Fluid.of("gtceu:lubricant", 500))
+        .EUt(240)
+        .duration(240);
+    greg.inscriber("gfs:stupid_print_engineering")
+        .itemInputs("#forge:plates/diamond")
+        .notConsumable("gfs:stupid_press")
+        .itemOutputs("ae2:printed_engineering_processor")
+        .inputFluids(Fluid.of("gtceu:lubricant", 500))
+        .EUt(240)
+        .duration(240);
+    greg.inscriber("gfs:stupid_print_accumulation")
+        .itemInputs("#forge:plates/black_steel")
+        .notConsumable("gfs:stupid_press")
+        .itemOutputs("ae2:printed_accumulation_processor")
+        .inputFluids(Fluid.of("gtceu:lubricant", 500))
+        .EUt(240)
+        .duration(240);
+	
 });

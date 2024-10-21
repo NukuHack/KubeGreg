2a25704b61edf54f27c761d293e959e473001a76
NukuHack <109027819+NukuHack@users.noreply.github.com>
2024-10-21 15:33:53 +0200

    Update singleblock_machines.js
    better input numbers

--- kubejs/startup_scripts/gtceu/singleblock_machines.js
+++ kubejs/startup_scripts/gtceu/singleblock_machines.js
@@ -9,7 +9,7 @@ GTCEuStartupEvents.registry("gtceu:recipe_type", event => {
     event.create("inscriber")
         .category("gfs")
         .setEUIO("in")
-        .setMaxIOSize(3, 1, 1, 0)
+        .setMaxIOSize(5, 2, 3, 0)
         .setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
         .setProgressBar(GuiTextures.PROGRESS_BAR_COMPRESS, FillDirection.LEFT_TO_RIGHT)
         .setSound(GTSoundEntries.COMPRESSOR);
@@ -18,7 +18,7 @@ GTCEuStartupEvents.registry("gtceu:recipe_type", event => {
     event.create("rock_generator")
         .category("gfs")
         .setEUIO("in")
-        .setMaxIOSize(3, 1, 2, 0)
+        .setMaxIOSize(3, 2, 2, 0)
         .setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
         .setProgressBar(GuiTextures.PROGRESS_BAR_COMPRESS, FillDirection.LEFT_TO_RIGHT)
         .setSound(GTSoundEntries.ELECTROLYZER);

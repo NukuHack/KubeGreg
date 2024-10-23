40c33c75ef0d84182ad7b73cfc2d9c8c9a73ebab
NukuHack <109027819+NukuHack@users.noreply.github.com>
2024-10-23 17:18:10 +0200

    Create recipe_shortening.js
    ff

---
+++ kubejs/server_scripts/big_mods/gtceu/recipe_shortening.js
@@ -0,0 +1,15 @@
+ServerEvents.recipes(event => {
+
+  // plan to shorten the LCR and Autoclave
+  /*
+    event.forEachRecipe({ mod: 'gtceu' }, recipe => {
+        try {
+            var newDuration = recipe.get("duration")
+            recipe.set("duration", newDuration/10)
+        } catch (err) {
+            console.log(recipe.id + " has no duration field, skipped.")
+        }
+    })
+  */
+  
+})

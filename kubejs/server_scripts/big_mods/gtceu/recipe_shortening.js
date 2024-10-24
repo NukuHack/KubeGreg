15ad871c45504a21471a5ddccb31f2a3834345b7
NukuHack <109027819+NukuHack@users.noreply.github.com>
2024-10-24 21:03:33 +0200

    Update recipe_shortening.js
    try - catch

--- kubejs/server_scripts/big_mods/gtceu/recipe_shortening.js
+++ kubejs/server_scripts/big_mods/gtceu/recipe_shortening.js
@@ -1,18 +1,24 @@
+priority: 1
+
 ServerEvents.recipes(event => {
 
   // plan to shorten the LCR and Autoclave
-  /*
-    event.forEachRecipe({mod:'gtceu',type:'autoclave'}, recipe => {
+  
+    event.forEachRecipe({mod:'gtceu',[{type:'autoclave'},{type:'electric_blast_furnace'}]}, recipe => {
         try {
-            var energy = recipe.get("EUt")
-            if (energy<480) { // it is not Hv or higher
-            var oldDuration = recipe.get("duration")
-            recipe.set("duration", oldDuration/10)
-            };
+            //var energy = recipe.get("EUt")
+            var oldDuration = recipe.get("duration");
+            var oldDS = oldDuration / 20;
+            if (oldDS>100)
+              recipe.set("duration", 100*20);
+            else if (oldDS>20)
+              recipe.set("duration", oldDuration/5);
+            else
+              recipe.set("duration", oldDuration/2);
         } catch (err) {
             console.log(recipe.id + " has no duration field, skipped.")
         };
     });
-  */
+  
   
 })

73eee81fb9a13811d716c4aa9a37e4d3f50d9f5c
NukuHack <109027819+NukuHack@users.noreply.github.com>
2024-10-28 20:03:10 +0100

    Update recipetype_modifications.js
    f

--- kubejs/startup_scripts/gtceu/recipetype_modifications.js
+++ kubejs/startup_scripts/gtceu/recipetype_modifications.js
@@ -1,7 +1,7 @@
 GTCEuStartupEvents.registry("gtceu:recipe_type", event => {
 
     // Alloy smelter (used to be 2,1,0,0)
-    GTRecipeTypes.ALLOY_SMELTER_RECIPES.setMaxIOSize(2, 2, 0,0)
+    GTRecipeTypes.ALLOY_SMELTER_RECIPES.setMaxIOSize(3, 2, 0,0)
     
     //GTRecipeTypes.ELECTRIC_BLAST_FURNACE.setMaxIOSize(6, 3, 3,3)
   

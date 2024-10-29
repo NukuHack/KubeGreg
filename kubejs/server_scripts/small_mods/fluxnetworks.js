e489c386ad0c9e367106b15fc9d60407da6c6521
NukuHack <109027819+NukuHack@users.noreply.github.com>
2024-10-29 20:52:53 +0100

    Update fluxnetworks.js
    qol

--- kubejs/server_scripts/small_mods/fluxnetworks.js
+++ kubejs/server_scripts/small_mods/fluxnetworks.js
@@ -2,14 +2,22 @@
 ServerEvents.recipes(event => {
 
 	// making it ..... obtainable in void world
+    const greg = event.recipes.gtceu;
 
-    event.recipes.gtceu.chemical_reactor('flux_dust')
+    greg.chemical_reactor('flux_dust')
     .itemInputs('8x #forge:dusts/redstone')
     .itemInputs('8x #forge:dusts/obsidian')
     .inputFluids('minecraft:lava 32000')
     .itemOutputs('16x fluxnetworks:flux_dust')
-    .duration(2400)
-    .EUt(120)
-    .circuit(1);
+    .duration(24*5*20)
+    .EUt(120);
+	
+    greg.large_chemical_reactor('flux_dust')
+    .itemInputs('8x #forge:dusts/redstone')
+    .itemInputs('8x #forge:dusts/obsidian')
+    .inputFluids('minecraft:lava 32000')
+    .itemOutputs('16x fluxnetworks:flux_dust')
+    .duration(24*5*20)
+    .EUt(120);
 
 });

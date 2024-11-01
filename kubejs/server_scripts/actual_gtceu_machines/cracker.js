5797f2de98b05d180c18c07f335ec29899d44958
NukuHack <nukuhack@gmail.com>
2024-11-01 15:45:52 +0100

    distillery not yet working but cracking is nice

--- kubejs/server_scripts/actual_gtceu_machines/cracker.js
+++ kubejs/server_scripts/actual_gtceu_machines/cracker.js
@@ -3,12 +3,30 @@ ServerEvents.recipes(event => {
     const greg = event.recipes.gtceu;
 
 
-    greg.cracker('gfs:ethylene_f_methane')
-        .inputFluids(Fluid.of("gtceu:methane", 2000),Fluid.of("gtceu:steam", 1000))
-        .outputFluids(Fluid.of("gtceu:ethylene", 1000),Fluid.of("gtceu:hydrogen", 6000))
+    greg.cracker('gfs:steam_methane')
+        .inputFluids(Fluid.of("gtceu:methane", 1000),
+            Fluid.of("gtceu:steam", 1000))
+        .outputFluids(Fluid.of("gtceu:steam_cracked_methane", 1000))
         .EUt(480*16) // Iv ?
         .circuit(6)
-        .duration(40*20);
+        .duration(5*20);
+
+    greg.cracker('gfs:hydrogen_methane')
+        .inputFluids(Fluid.of("gtceu:methane", 1000),
+            Fluid.of("gtceu:hydrogen", 6000))
+        .outputFluids(Fluid.of("gtceu:severly_hydrocracked_methane", 1000))
+        .EUt(480*16) // Iv ?
+        .circuit(6)
+        .duration(8*20);
+
+    greg.cracker('gfs:double_methane')
+        .inputFluids(Fluid.of("gtceu:severly_hydrocracked_methane", 1000),
+            Fluid.of("gtceu:carbon_monoxide", 2000))
+        .outputFluids(Fluid.of("gtceu:double_cracked_methane", 1000))
+        .EUt(480*16) // Iv ?
+        .circuit(6)
+        .duration(10*20);
+
 
 
 });
---
+++ kubejs/server_scripts/actual_gtceu_machines/distill.js
@@ -0,0 +1,46 @@
+ServerEvents.recipes(event => {
+
+    const greg = event.recipes.gtceu;
+
+
+    /*
+    gtceu:steam_cracked_methane
+
+    48% ethylene
+    24% hydrogen
+    10% propane
+    10% propene
+    10% butane
+    */
+
+    /*
+    severly_hydrocracked_methane
+
+    35% propane
+    25% ethane
+    25% ethylene
+    15% C3H6
+    20% butane
+    */
+
+    /*
+    gtceu:double_cracked_methane
+
+    300% hydrogen
+    200% ethane
+    100% ethylene
+    100% propane
+    100% butane
+    50% C3H6
+    30% benzene
+    30% toluene
+    */
+
+
+    /*
+    butane
+
+    60% butene
+    40% butadiene
+    */
+});

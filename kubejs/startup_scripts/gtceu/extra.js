107e57b134e0b1f1e3be8a0d2c4950dd553ec7dd
NukuHack <nukuhack@gmail.com>
2024-11-02 18:17:55 +0100

    ff

--- kubejs/startup_scripts/gtceu/extra.js
+++ kubejs/startup_scripts/gtceu/extra.js
@@ -3,8 +3,7 @@ GTCEuStartupEvents.materialModification(event => {
 const $FluidProperty = Java.loadClass('com.gregtechceu.gtceu.api.data.chemical.material.properties.FluidProperty');
 const $FluidBuilder = Java.loadClass('com.gregtechceu.gtceu.api.fluids.FluidBuilder');
 const $FluidStorageKeys = Java.loadClass('com.gregtechceu.gtceu.api.fluids.store.FluidStorageKeys');
-*/
-/*
+
 	let property = material.getProperty(PropertyKey.FLUID);
     property.getStorage().store(FluidStorageKeys.LIQUID, () -> Fluid.getType("theurgy:sal_ammoniac"), null);
 */

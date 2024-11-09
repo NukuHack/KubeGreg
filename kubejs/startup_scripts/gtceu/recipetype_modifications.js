c1137a7a3528e7098c1a3ee8fbcf19f1ff17fcab
NukuHack <nukuhack@gmail.com>
2024-11-09 18:24:08 +0100

    changed some things

--- kubejs/startup_scripts/gtceu/recipetype_modifications.js
+++ kubejs/startup_scripts/gtceu/recipetype_modifications.js
@@ -1,8 +1,10 @@
 GTCEuStartupEvents.registry("gtceu:recipe_type", event => {
 
     // Alloy smelter (used to be 2,1,0,0)
-    GTRecipeTypes.ALLOY_SMELTER_RECIPES.setMaxIOSize(3, 2, 0,0)
+    GTRecipeTypes.ALLOY_SMELTER_RECIPES.setMaxIOSize(3, 2, 0, 0)
     // Emectric Blast FUrnace (used to be 3,3,1,1)
-    GTRecipeTypes.BLAST_RECIPES.setMaxIOSize(3, 3, 3,3)
+    GTRecipeTypes.BLAST_RECIPES.setMaxIOSize(3, 3, 3, 3)
+    // Compressor (used to be 1,1,0,0)
+    GTRecipeTypes.COMPRESSOR.setMaxIOSize(2, 2, 1, 1)
 
 });

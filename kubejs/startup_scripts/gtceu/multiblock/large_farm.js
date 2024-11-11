f6511d198d52f97d4e8de97f569101f305e2b316
NukuHack <nukuhack@gmail.com>
2024-11-11 20:43:30 +0100

    made it look actually better than the simple version

--- kubejs/startup_scripts/gtceu/multiblock/large_farm.js
+++ kubejs/startup_scripts/gtceu/multiblock/large_farm.js
@@ -10,7 +10,7 @@ GTCEuStartupEvents.registry('gtceu:machine', event => {
 	event.create('large_farm', 'multiblock')
 		.rotationState(RotationState.NON_Y_AXIS)
 		.recipeType('large_farm')
-		.appearanceBlock(GTBlocks.TREATED_WOOD_PLANK)
+		.appearanceBlock(GTBlocks.CASING_STEEL_SOLID)
 		.recipeModifiers([GTRecipeModifiers.PARALLEL_HATCH,
 			GTRecipeModifiers.ELECTRIC_OVERCLOCK.apply(OverclockingLogic.PERFECT_OVERCLOCK)])
 		.pattern(definition => FactoryBlockPattern.start()
@@ -18,26 +18,26 @@ GTCEuStartupEvents.registry('gtceu:machine', event => {
 			.aisle('PDDDP', 'F   F', 'F   F', 'FGGGF').setRepeatable(12)
 			.aisle('WEEEW', 'FFCFF', 'FFFFF', 'FFFFF')
 			.where('C', Predicates.controller(Predicates.blocks(definition.get())))
-			.where('P', Predicates.blocks(GTBlocks.TREATED_WOOD_PLANK.get())
+			.where('P', Predicates.blocks(GTBlocks.CASING_STEEL_SOLID.get())
 				.or(Predicates.abilities(PartAbility.IMPORT_FLUIDS).setMaxGlobalLimited(12))
 				.or(Predicates.abilities(PartAbility.IMPORT_ITEMS).setMaxGlobalLimited(12))
 				
 				.or(Predicates.abilities(PartAbility.EXPORT_FLUIDS).setMaxGlobalLimited(12))
 				.or(Predicates.abilities(PartAbility.EXPORT_ITEMS).setMaxGlobalLimited(12))
 			)
-			.where('E', Predicates.blocks(GTBlocks.TREATED_WOOD_PLANK.get())
+			.where('E', Predicates.blocks(GTBlocks.CASING_STEEL_SOLID.get())
 				.or(Predicates.abilities(PartAbility.MAINTENANCE).setExactLimit(1))
 				.or(Predicates.abilities(PartAbility.INPUT_ENERGY).setMinGlobalLimited(1).setMaxGlobalLimited(2))
 				.or(Predicates.abilities(PartAbility.PARALLEL_HATCH).setMaxGlobalLimited(1))
 			)
-			.where('W', Predicates.blocks(GTBlocks.TREATED_WOOD_PLANK.get()))
+			.where('W', Predicates.blocks(GTBlocks.CASING_STEEL_SOLID.get()))
 			.where('G', Predicates.blocks(GTBlocks.CASING_TEMPERED_GLASS.get()))
 			.where('D', Predicates.blocks('gfs:magical_farmland'))
-			.where('F', Predicates.blocks('gtceu:treated_wood_frame'))
+			.where('F', Predicates.blocks('gtceu:steel_frame'))
 			.where(' ', Predicates.any())
 			.build())
 		.workableCasingRenderer(
-			"gtceu:block/treated_wood_planks",
-			"gtceu:block/multiblock/coke_oven", false
+			"gtceu:block/casings/solid/machine_casing_solid_steel",
+			"gtceu:block/multiblock/implosion_compressor", false
 		);
 });

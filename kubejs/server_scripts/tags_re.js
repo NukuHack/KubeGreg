aed223da727ca4a7e006f554ad7ebb9a38c37a3c
NukuHack <nukuhack@gmail.com>
2024-11-11 18:43:12 +0100

    making tag removing actually work

--- kubejs/server_scripts/tags_re.js
+++ kubejs/server_scripts/tags_re.js
@@ -2,10 +2,17 @@ priority: 60
 
 ServerEvents.tags('item', event => {
 
-	event.remove(["minecraft:planks","forge:planks/wood"], "gtceu:treated_wood_planks");
+function rem_help(arr,x) {
+	arr.forEach((y)=>{
+		event.remove(y,x)
+	});
+};
 
-	event.remove(["forge:dye","forge:dye/red","forge:red_dye","c:dyes"], "minecraft:redstone");
-	event.remove(["forge:dye","forge:dye/grey","forge:grey_dye","c:dyes"], "gtceu:metal_mixture_dust");
+
+	rem_help(["minecraft:planks","forge:planks/wood"], "gtceu:treated_wood_planks");
+
+	rem_help(["forge:dyes","forge:dyes/red","c:dyes","c:red_dyes"], "minecraft:redstone");
+	rem_help(["forge:dyes","forge:brown/grey","c:dyes","c:brown_dyes"], "gtceu:metal_mixture_dust");
 
 	replacements.forEach((id) => {
 		event.remove(id[0], id[1]);

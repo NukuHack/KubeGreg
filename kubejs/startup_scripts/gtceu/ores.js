79fd69c117fbdc2bfdf5e7e69d112aa3d01476ca
NukuHack <nukuhack@gmail.com>
2024-11-14 21:48:45 +0100

    separate them in order to make it clearer and easier to make an order (so they will not try to create before the other elements)

---
+++ kubejs/startup_scripts/gtceu/ores.js
@@ -0,0 +1,33 @@
+
+priority: -50
+
+GTCEuStartupEvents.registry("gtceu:material", event => {
+
+    event.create("dilithium")
+        .dust()
+        .ore(2, 1)
+        .color(0xd1b5b4)
+        .iconSet(CERTUS)
+        .components('2x lithium')
+        .addOreByproducts('lithium', 'cobalt', 'platinum')
+
+    event.create("fluorite") // Hardmode only
+        .dust().ore()
+        .color(0xFFFC9E).iconSet('rough')
+        .components('calcium', '2x fluorine')
+        .addOreByproducts('sphalerite', 'bastnasite', 'topaz')
+
+    event.create("dulysite") // Hardmode only
+        .dust().ore(2, 1)
+        .iconSet('dull')
+        .components('duranium', '3x chlorine')
+        .addOreByproducts('sphalerite', 'duranium', 'europium')
+
+    event.create("snowchestite") // Hardmode only
+        .dust().ore()
+        .color(0x274c9f).iconSet('shiny')
+        .flags(GTMaterialFlags.DISABLE_DECOMPOSITION)
+        .components('3x naquadah_oxide', 'pyromorphite')
+        .addOreByproducts('chalcopyrite', 'vanadium_magnetite', 'naquadah_hydroxide')
+
+});

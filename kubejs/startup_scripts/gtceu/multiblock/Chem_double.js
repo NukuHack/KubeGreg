9013c4f4afc53146ca6bebd60cb36725b414927c
NukuHack <nukuhack@gmail.com>
2024-11-14 17:26:49 +0100

    will use it for chemical materialization and chemical recipes like connecting atoms together in a (maybe even) unrealistic way

---
+++ kubejs/startup_scripts/gtceu/multiblock/Chem_double.js
@@ -0,0 +1,70 @@
+
+GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
+    event.create('chemical_plant')
+        .category('chemical_plant')
+        .setEUIO('in')
+        .setMaxIOSize(9, 9, 9, 9)
+        .setSound(GTSoundEntries.CHEMICAL);
+});
+
+GTCEuStartupEvents.registry('gtceu:machine', event => {
+    event.create('chemical_plant', 'multiblock')
+        .rotationState(RotationState.NON_Y_AXIS)
+        .recipeType('chemical_plant')
+        .recipeModifiers([GTRecipeModifiers.PARALLEL_HATCH, GTRecipeModifiers.ELECTRIC_OVERCLOCK.apply(OverclockingLogic.PERFECT_OVERCLOCK)])
+        .pattern(definition => FactoryBlockPattern.start()
+            .aisle('FHHHF', 'TFFFT', 'T   T', 'T   T', 'T   T', 'FFFFF')
+            .aisle('HFFFH', 'FPPPF', ' FFF ', ' MMM ', ' FFF ', 'FEEEF')
+            .aisle('HFFFH', 'FP PF', ' F F ', ' M M ', ' F F ', 'FEEEF')
+            .aisle('HFFFH', 'FPPPF', ' FFF ', ' MMM ', ' FFF ', 'FEEEF')
+            .aisle('FHHHF', 'TFCFT', 'T   T', 'T   T', 'T   T', 'FFFFF')
+            .where('C', Predicates.controller(Predicates.blocks(definition.get())))
+            .where('F', Predicates.blocks('kubejs:peek_casing')
+                .or(Predicates.autoAbilities(definition.getRecipeTypes()))
+                .or(Predicates.abilities(PartAbility.MAINTENANCE).setExactLimit(1))
+                .or(Predicates.abilities(PartAbility.PARALLEL_HATCH).setMaxGlobalLimited(1)))
+            .where('E', Predicates.blocks(GTBlocks.CASING_EXTREME_ENGINE_INTAKE.get()))
+            .where('H', Predicates.blocks(GCyMBlocks.HEAT_VENT.get()))
+            .where('M', Predicates.blocks(GCyMBlocks.MOLYBDENUM_DISILICIDE_COIL_BLOCK.get()))
+            .where('P', Predicates.blocks(GTBlocks.CASING_POLYTETRAFLUOROETHYLENE_PIPE.get()))
+            .where('T', Predicates.blocks('gtceu:tungsten_frame'))
+            .where(' ', Predicates.any())
+            .build())
+        .workableCasingRenderer("kubejs:block/casings/machine_casing_peek",
+            "gtceu:block/multiblock/large_chemical_reactor", false);
+});
+
+
+GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
+    event.create('chemical_builder')
+        .category('chemical_builder')
+        .setEUIO('in')
+        .setMaxIOSize(3, 1, 3, 1)
+        .setSound(GTSoundEntries.CHEMICAL);
+});
+
+GTCEuStartupEvents.registry('gtceu:machine', event => {
+    event.create('chemical_builder', 'multiblock')
+        .rotationState(RotationState.NON_Y_AXIS)
+        .recipeType('chemical_builder')
+        .recipeModifiers([GTRecipeModifiers.PARALLEL_HATCH, GTRecipeModifiers.ELECTRIC_OVERCLOCK.apply(OverclockingLogic.PERFECT_OVERCLOCK)])
+        .pattern(definition => FactoryBlockPattern.start()
+            .aisle('TFFFT', 'T   T', 'T   T', 'T   T', 'T   T', 'T   T', 'TFFFT')
+            .aisle('FKKKF', ' KKK ', '  K  ', '     ', '  K  ', ' KKK ', 'FEEEF')
+            .aisle('FKKKF', ' K K ', ' K K ', '  P  ', ' K K ', ' K K ', 'FEEEF')
+            .aisle('FKKKF', ' KCK ', '  K  ', '     ', '  K  ', ' KKK ', 'FEEEF')
+            .aisle('TFFFT', 'T   T', 'T   T', 'T   T', 'T   T', 'T   T', 'TFFFT')
+            .where('C', Predicates.controller(Predicates.blocks(definition.get())))
+            .where('K', Predicates.blocks('kubejs:peek_casing')
+                .or(Predicates.autoAbilities(definition.getRecipeTypes()))
+                .or(Predicates.abilities(PartAbility.MAINTENANCE).setExactLimit(1))
+                .or(Predicates.abilities(PartAbility.PARALLEL_HATCH).setMaxGlobalLimited(1)))
+            .where('E', Predicates.blocks(GTBlocks.CASING_EXTREME_ENGINE_INTAKE.get()))
+            .where('F', Predicates.blocks(GTBlocks.FIREBOX_TUNGSTENSTEEL.get()))
+            .where('P', Predicates.blocks(GTBlocks.CASING_POLYTETRAFLUOROETHYLENE_PIPE.get()))
+            .where('T', Predicates.blocks('gtceu:trinaquadalloy_frame'))
+            .where(' ', Predicates.any())
+            .build())
+        .workableCasingRenderer("kubejs:block/casings/machine_casing_peek",
+            "gtceu:block/multiblock/large_chemical_reactor", false);
+});

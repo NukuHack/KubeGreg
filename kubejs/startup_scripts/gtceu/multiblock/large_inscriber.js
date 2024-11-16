e6ebd98e03b65678f6e90c1ad609c6caa9f96e4c
NukuHack <nukuhack@gmail.com>
2024-11-16 17:32:09 +0100

    moved large_inscriber

--- kubejs/startup_scripts/gtceu/multiblock/large_inscriber.js
+++ kubejs/startup_scripts/gtceu/multiblock/large_inscriber.js
@@ -14,28 +14,26 @@ GTCEuStartupEvents.registry('gtceu:machine', event => {
     event.create('large_inscriber', 'multiblock')
         .rotationState(RotationState.NON_Y_AXIS)
         .recipeType(GTRecipeTypes.get("inscriber"))
-         // gtceu: / gfs:
         .appearanceBlock(GTBlocks.CASING_STAINLESS_CLEAN)
         .recipeModifiers([GTRecipeModifiers.PARALLEL_HATCH,
             GTRecipeModifiers.ELECTRIC_OVERCLOCK.apply(OverclockingLogic.PERFECT_OVERCLOCK)])
         .pattern(definition => FactoryBlockPattern.start()
             .aisle('EEE', 'HHH', 'HHH')
-            .aisle('EEE', 'HGH', 'HGH')
-            .aisle('ECE', 'HGH', 'HGH')
+            .aisle('EEE', 'FGF', 'FGF')
+            .aisle('ECE', 'FGF', 'FGF')
             .where('C', Predicates.controller(Predicates.blocks(definition.get())))
             .where('E', Predicates.blocks(GTBlocks.CASING_STAINLESS_CLEAN.get())
                 .or(Predicates.abilities(PartAbility.MAINTENANCE).setExactLimit(1))
                 .or(Predicates.abilities(PartAbility.INPUT_ENERGY).setMinGlobalLimited(1).setMaxGlobalLimited(2))
                 .or(Predicates.abilities(PartAbility.PARALLEL_HATCH).setMaxGlobalLimited(1))
+                .or(Predicates.abilities(PartAbility.IMPORT_FLUIDS))
+                .or(Predicates.abilities(PartAbility.IMPORT_ITEMS))
+
+                .or(Predicates.abilities(PartAbility.EXPORT_FLUIDS))
+                .or(Predicates.abilities(PartAbility.EXPORT_ITEMS))
             )
             .where('G', Predicates.blocks(GTBlocks.CASING_TEMPERED_GLASS.get()))
-            .where('H', Predicates.blocks(GTBlocks.CASING_STAINLESS_CLEAN.get())
-                .or(Predicates.abilities(PartAbility.IMPORT_FLUIDS).setMaxGlobalLimited(6))
-                .or(Predicates.abilities(PartAbility.IMPORT_ITEMS).setMaxGlobalLimited(6))
-
-                .or(Predicates.abilities(PartAbility.EXPORT_FLUIDS).setMaxGlobalLimited(6))
-                .or(Predicates.abilities(PartAbility.EXPORT_ITEMS).setMaxGlobalLimited(6))
-            )
+            .where('F', Predicates.blocks('gtceu:laser_safe_engraving_casing'))
             .build())
         .workableCasingRenderer(
             "gtceu:block/casings/solid/machine_casing_clean_stainless_steel",

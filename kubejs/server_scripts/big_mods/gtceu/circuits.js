92f2eb69e0c75e13ab232d8cba4907f691e9f19a
NukuHack <nukuhack@gmail.com>
2024-11-17 18:12:18 +0100

    replaced original (non-existing) plastic with PEEK

--- kubejs/server_scripts/big_mods/gtceu/circuits.js
+++ kubejs/server_scripts/big_mods/gtceu/circuits.js
@@ -2,11 +2,11 @@
 ServerEvents.recipes(event => {
 
     const greg = event.recipes.gtceu;
-
+// tiers : zmp,uv,uhv,uev,uiv,ux,opv,max
 // Wetware tweaks
     event.remove({output: 'gtceu:neuro_processing_unit'})
-    greg.circuit_assembler('neuro_processing_unit')
-        .itemInputs('gtceu:wetware_printed_circuit_board', '4x gtceu:stem_cells', '8x gtceu:polybenzimidazole_small_fluid_pipe', '4x gtceu:enderium_plate', '16x gtceu:silicone_rubber_foil', '8x gtceu:hsse_bolt')
+    greg.circuit_assembler('gfs:neuro_processing_unit')
+        .itemInputs('gtceu:wetware_printed_circuit_board', '4x gtceu:stem_cells', '8x gtceu:polybenzimidazole_small_fluid_pipe', '4x gtceu:neutronium_plate', '16x gtceu:silicone_rubber_foil', '8x gtceu:hsse_bolt')
         .inputFluids('gtceu:sterilized_growth_medium 100')
         .itemOutputs('gtceu:neuro_processing_unit')
         .duration(600)
@@ -15,7 +15,7 @@ ServerEvents.recipes(event => {
 
 // WIP: Matter Circuits
     greg.circuit_assembler('matter_circuit_board')
-        .itemInputs('16x gtceu:omnium_plate', '32x gtceu:activated_netherite_foil', '32x gtceu:crystal_matrix_foil', '#gtceu:circuits/luv', 'gtceu:iv_emitter',
+        .itemInputs('16x gtceu:omnium_plate', '32x gtceu:activated_netherite_foil', '32x gtceu:crystal_matrix_foil', '4x gtceu:neuro_processing_unit', 'gtceu:iv_emitter',
             '8x gtceu:omnic_acid_dust')
         .inputFluids('minecraft:water 500')
         .itemOutputs('16x gfs:matter_circuit_board')
@@ -48,7 +48,7 @@ ServerEvents.recipes(event => {
     greg.assembly_line('matter_processor_computer')
         .itemInputs('gfs:matter_processing_unit', '2x gfs:matter_processor_assembly', '24x gfs:multidimensional_cpu_chip',
             '12x gtceu:advanced_smd_capacitor', '8x gtceu:advanced_smd_transistor', '8x gtceu:advanced_smd_diode', '8x gtceu:advanced_smd_resistor',
-            '8x gtceu:uhpic_chip', '4x gtceu:enriched_naquadah_trinium_europium_duranide_double_wire', '8x gtceu:polyethyl_cyanoacrylate_foil')
+            '8x gtceu:uhpic_chip', '4x gtceu:enriched_naquadah_trinium_europium_duranide_double_wire', '8x gtceu:polyether_ether_ketone_foil')
         .inputFluids('gtceu:soldering_alloy 1152', 'gtceu:omnium 144')
         .itemOutputs('gfs:matter_processor_computer')
         .duration(200)
@@ -57,7 +57,7 @@ ServerEvents.recipes(event => {
     greg.assembly_line('matter_processor_mainframe')
         .itemInputs('4x gtceu:activated_netherite_frame', '16x gfs:matter_processor_computer', '24x gtceu:advanced_smd_capacitor',
             '24x gtceu:advanced_smd_transistor', '24x gtceu:advanced_smd_diode', '24x gtceu:advanced_smd_resistor', '24x gtceu:advanced_smd_inductor',
-            '32x gtceu:ram_chip', '16x gtceu:polyethyl_cyanoacrylate_foil', '8x gtceu:enriched_naquadah_trinium_europium_duranide_double_wire',
+            '32x gtceu:ram_chip', '16x gtceu:polyether_ether_ketone_foil', '8x gtceu:enriched_naquadah_trinium_europium_duranide_double_wire',
             '8x gtceu:crystal_matrix_plate') // could replace with omnium frame
         .inputFluids('gtceu:soldering_alloy 1152', 'gtceu:omnium 288')
         .itemOutputs('gfs:matter_processor_mainframe')
@@ -84,7 +84,7 @@ ServerEvents.recipes(event => {
     const smds = [
         ['transistor', 'gtceu:diamond_plate', '8x gtceu:fine_holmium_wire'],
         ['resistor', 'gtceu:activated_netherite_dust', '4x gtceu:fine_holmium_wire'],
-        ['capacitor', 'gtceu:polyethyl_cyanoacrylate_foil', 'gtceu:holmium_foil'],
+        ['capacitor', 'gtceu:polyether_ether_ketone_foil', 'gtceu:holmium_foil'],
         ['diode', 'gtceu:ruthenium_trinium_americium_neutronate_dust', '16x gtceu:fine_holmium_wire'],
         ['inductor', 'gtceu:tritanium_ring', '16x gtceu:fine_holmium_wire']
     ]
@@ -116,7 +116,7 @@ ServerEvents.recipes(event => {
     greg.assembly_line('dimensional_processor_computer')
         .itemInputs('gfs:dimensional_processing_unit', '4x gfs:dimensional_processor_assembly', '8x gfs:multidimensional_cpu_chip',
             '16x gfs:complex_smd_capacitor', '16x gfs:complex_smd_transistor', '8x gfs:complex_smd_diode', '8x gfs:complex_smd_resistor',
-            '16x gtceu:uhpic_chip', '4x gtceu:ruthenium_trinium_americium_neutronate_double_wire',  '16x gtceu:polyethyl_cyanoacrylate_foil')
+            '16x gtceu:uhpic_chip', '4x gtceu:ruthenium_trinium_americium_neutronate_double_wire',  '16x gtceu:polyether_ether_ketone_foil')
         .inputFluids('gtceu:soldering_alloy 1152')
         .itemOutputs('gfs:dimensional_processor_computer')
         .duration(200)
@@ -125,7 +125,7 @@ ServerEvents.recipes(event => {
     greg.assembly_line('dimensional_processor_mainframe')
         .itemInputs('4x gtceu:infinity_frame', '16x gfs:dimensional_processor_computer', '32x gfs:complex_smd_capacitor',
             '32x gfs:complex_smd_transistor', '32x gfs:complex_smd_diode', '32x gfs:complex_smd_resistor', '32x gfs:complex_smd_inductor',
-            '32x gfs:hyperdynamic_ram_chip', '32x gtceu:polyethyl_cyanoacrylate_foil', '8x gtceu:ruthenium_trinium_americium_neutronate_double_wire', '4x gtceu:double_holmium_plate') // could replace with omnium frame
+            '32x gfs:hyperdynamic_ram_chip', '32x gtceu:polyether_ether_ketone_foil', '8x gtceu:ruthenium_trinium_americium_neutronate_double_wire', '4x gtceu:double_holmium_plate') // could replace with omnium frame
         .inputFluids('gtceu:soldering_alloy 1152')
         .itemOutputs('gfs:dimensional_processor_mainframe')
         .duration(1200)
@@ -167,7 +167,7 @@ ServerEvents.recipes(event => {
     greg.assembly_line('monic_processor_computer')
         .itemInputs('gfs:monic_processing_unit', '2x gfs:monic_processor_assembly', '4x gfs:contained_singularity',
             '16x gfs:complex_smd_capacitor', '16x gfs:complex_smd_transistor', '16x gfs:complex_smd_diode', '16x gfs:complex_smd_resistor',
-            '16x gtceu:uhpic_chip', '4x gtceu:activated_netherite_double_wire',  '32x gtceu:polyethyl_cyanoacrylate_foil')
+            '16x gtceu:uhpic_chip', '4x gtceu:activated_netherite_double_wire',  '32x gtceu:polyether_ether_ketone_foil')
         .inputFluids('gtceu:soldering_alloy 1152')
         .itemOutputs('2x gfs:monic_processor_computer')
         .duration(200)
@@ -177,7 +177,7 @@ ServerEvents.recipes(event => {
         .itemInputs('8x gtceu:monium_frame', '2x gfs:monic_processing_unit', '16x gfs:monic_processor_computer', '64x gfs:complex_smd_capacitor', '64x gfs:complex_smd_transistor',
             '64x gfs:complex_smd_diode', '64x gfs:complex_smd_resistor', '64x gfs:complex_smd_inductor',
             '64x gfs:contained_singularity', '64x gfs:contained_singularity', '64x gfs:multidimensional_cpu_chip',
-            '64x gfs:hyperdynamic_ram_chip', '64x gtceu:polyethyl_cyanoacrylate_foil', '64x gfs:quantum_soc_chip', '4x gtceu:double_infinity_plate', '8x gtceu:sculk_superconductor_double_wire') // could replace with omnium frame
+            '64x gfs:hyperdynamic_ram_chip', '64x gtceu:polyether_ether_ketone_foil', '64x gfs:quantum_soc_chip', '4x gtceu:double_infinity_plate', '8x gtceu:sculk_superconductor_double_wire') // could replace with omnium frame
         .inputFluids('gtceu:soldering_alloy 1152')
         .itemOutputs('gfs:monic_processor_mainframe')
         .duration(1200)

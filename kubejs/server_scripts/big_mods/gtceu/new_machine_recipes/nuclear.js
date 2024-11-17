1e221c01ccfa4c473c6039701477a24bb5948269
NukuHack <nukuhack@gmail.com>
2024-11-17 18:09:43 +0100

    nuclear plans

---
+++ kubejs/server_scripts/big_mods/gtceu/new_machine_recipes/nuclear.js
@@ -0,0 +1,30 @@
+/*
+    stuff
+*/
+
+ServerEvents.recipes(event => {
+
+  const greg = event.recipes.gtceu;
+
+	// reactor make radiactive stuff + water = non radioactive stuff + radioactive steam
+	// turbine make radioactive steam = water
+
+/*
+  greg
+      .nuclear_reactor("gfs:x")
+      .itemInputs('x')
+      .inputFluids(Fluid.of('x', 2880))
+      .itemOutputs('x')
+      .duration(x)
+      .EUt(x);
+  greg
+      .nuclear_turbine("gfs:x")
+      .itemInputs('x')
+      .inputFluids(Fluid.of('x', 2880))
+      .itemOutputs('x')
+      .duration(x)
+      .EUt(x);
+*/
+
+
+});

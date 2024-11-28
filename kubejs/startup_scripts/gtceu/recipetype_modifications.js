b11e4e36f5e655b0a4c3196dd5bf3da2ffa12af3
NukuHack <nukuhack@gmail.com>
2024-11-28 17:16:55 +0100

    some new recipetype modifications

--- kubejs/startup_scripts/gtceu/recipetype_modifications.js
+++ kubejs/startup_scripts/gtceu/recipetype_modifications.js
@@ -6,5 +6,9 @@ GTCEuStartupEvents.registry("gtceu:recipe_type", event => {
     GTRecipeTypes.BLAST_RECIPES.setMaxIOSize(3, 3, 3, 3)
     // Compressor (used to be 1,1,0,0)
     GTRecipeTypes.COMPRESSOR_RECIPES.setMaxIOSize(2, 2, 1, 1)
-
+    // Mixer (used to be 6,3,1,1)
+    GTRecipeTypes.MIXER_RECIPES.setMaxIOSize(6, 3, 3, 1)
+    // GTRecipeTypes.FORGE_HAMMER_RECIPES.setMaxIOSize(1, 3, 0, 0) // FOR SOME REASON I REFUSES TO WORK!
+    GTRecipeTypes.FUSION_RECIPES.setMaxIOSize(3, 3, 6, 6)
+    // GTRecipeTypes.CENTRIFUGE_RECIPES.setMaxIOSize(9, 4, 6, 5)
 });

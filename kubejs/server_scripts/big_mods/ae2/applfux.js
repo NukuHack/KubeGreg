4516111af13d559f69aeb0f4836e0b1cc94184ef
NukuHack <nukuhack@gmail.com>
2024-11-29 21:03:09 +0100

    removed everything from appflux mod (will add them back manually later)

---
+++ kubejs/server_scripts/big_mods/ae2/applfux.js
@@ -0,0 +1,13 @@
+
+ServerEvents.recipes(event => {
+
+	const greg = event.recipes.gtceu;
+
+	event.remove({ mod:"appflux" });
+	
+	event.shapeless("appflux:part_flux_accessor","appflux:flux_accessor");
+	event.shapeless("appflux:flux_accessor","appflux:part_flux_accessor");
+	
+	// need to make the cells and the inscriber stuff, also the storage and energy acceptor and the card
+	
+});

f32e4c685803296c23b700bb7016d57c63ac48a7
NukuHack <nukuhack@gmail.com>
2024-11-29 17:49:15 +0100

    bigger input and output for inscriber

--- kubejs/startup_scripts/gtceu/singleblock_machines.js
+++ kubejs/startup_scripts/gtceu/singleblock_machines.js
@@ -7,7 +7,7 @@ GTCEuStartupEvents.registry("gtceu:recipe_type", event => {
     event.create("inscriber")
         .category("gfs")
         .setEUIO("in")
-        .setMaxIOSize(4, 1, 2, 0)
+        .setMaxIOSize(6, 2, 3, 0)
         .setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
         .setProgressBar(GuiTextures.PROGRESS_BAR_COMPRESS, FillDirection.LEFT_TO_RIGHT)
         .setSound(GTSoundEntries.COMPRESSOR);

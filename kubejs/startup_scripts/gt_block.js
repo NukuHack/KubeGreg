c4ef304618006576af12bc9deaf4d42a25aa8de0
NukuHack <nukuhack@gmail.com>
2024-11-29 21:05:03 +0100

    removed comments and changes the texture location of the things (also made some name a bit better, need to fix multiblocks now)

--- kubejs/startup_scripts/gt_block.js
+++ kubejs/startup_scripts/gt_block.js
@@ -26,13 +26,16 @@ StartupEvents.registry("block", event => {
     */
 
     const casingS = [
-        ["test","Test",10,1,1,"metal","gtceu:block/casings/test_casing"],
-        ["gren","Agri",4,1,10,"metal",'gtceu:block/casings/gren_casing'],
-        ["netherite","Netherite",5,1,0,"metal","gfs:block/casings/machine_casing_peek"],
-        ["peek","PEEK",7,2,0,"metal","gfs:block/casings/machine_casing_netherite"],
-        ["omnic_matrix","Omnic Matrix",8,1,0,"metal","gfs:block/casings/machine_casing_omnic_matrix"],
-        ['austenitic_stainless_steel_304','Austenitic Stainless Steel 304 Nuclear',6,2,0,"metal",'gfs:block/casings/machine_casing_a_s_steel'],
-        ['inconel_625','Inconel 625 Thermal Fluctuation Resistant',5,3,0,"metal",'gfs:block/casings/machine_casing_inconel'],
+		// name , Displayname, hard, resistance, light, sound, texture
+        ["test","Test",10,1,1,"metal","gfs:block/casings/test_casing"],
+        ["green","Agri",4,1,10,"metal",'gfs:block/casings/green_casing'],
+        ["yellow","Yellow",5,1,15,"metal",'gfs:block/casings/yellow_casing'],
+        ["netherite","Netherite",5,1,0,"metal","gfs:block/casings/peek"],
+        ["peek","PEEK",7,2,0,"metal","gfs:block/casings/netherite"],
+        ["omnic","Omnic Matrix",8,1,0,"metal","gfs:block/casings/omnic"],
+        ["high_temperature","High Temperature Fusion Casing (MKII)",10,2,0,"metal","gfs:block/casings/high_temperature"],
+        ['steel_304','Austenitic Stainless Steel 304 Nuclear',6,2,0,"metal",'gfs:block/casings/hard_steel'],
+        ['inconel_625','Inconel 625 Thermal Fluctuation Resistant',5,3,0,"metal",'gfs:block/casings/inconel'],
     ];
     casingS.forEach(([name,Dname,hard,res,light,sound,texture])=>{
         event.create(`gfs:${name}_casing`)
@@ -82,13 +85,12 @@ StartupEvents.registry("block", event => {
 
 
     const coils = [
-        ['mithril_coil_block', 12600, 16, 16, 8, 'mithril', 'gtceu:block/coils/machine_coil_mithril'],
-        ['omnic_matrix_coil_block', 15000, 24, 16, 9, 'omnium', 'gfs:block/coils/machine_coil_omnic_matrix'],
-        //['chadium_coil_block', 14400, 32, 32, 9, 'chadium', 'gtceu:block/coils/machine_coil_chadium'],
-        //['zalloy_coil_block', 13499, 24, 16, 10, 'zalloy', 'gfs:block/coils/machine_coil_zalloy'],
-        //['awakened_draconium_coil_block', 16200, 32, 32, 9, 'awakened_draconium', 'gtceu:block/coils/machine_coil_awakened_draconium'],
-        ['stellar_matter_coil_block', 18000, 32, 32, 9, 'stellar_matter', 'gtceu:block/coils/machine_coil_stellar_matter'],
-        ['infinity_coil_block', 19800, 32, 32, 9, 'infinity', 'gtceu:block/coils/machine_coil_infinity']
+        ['mithril_coil_block', 12600, 16, 16, 8, 'mithril', 'gfs:block/coils/mithril/mithril'],
+        ['omnic_matrix_coil_block', 15000, 24, 16, 9, 'omnium', 'gfs:block/coils/omnic/omnic'],
+        ['chadium_coil_block', 14400, 32, 32, 9, 'chadium', 'gtceu:block/coils/chad/chad'],
+        ['awakened_draconium_coil_block', 16200, 32, 32, 9, 'awakened_draconium', 'gtceu:block/coils/awakened_draconium/awakened_draconium'],
+        ['stellar_matter_coil_block', 18000, 32, 32, 9, 'stellar_matter', 'gtceu:block/coils/stellar/stellar'],
+        ['infinity_coil_block', 19800, 32, 32, 9, 'infinity', 'gtceu:block/coils/inf/inf']
     ];
 
     coils.forEach(([name, temperature, level, energyDiscount, tier, material, texture]) => {

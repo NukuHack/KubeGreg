8cf8c342584c7b50443aac1e174324f340fe9165
NukuHack <nukuhack@gmail.com>
2024-11-30 12:57:08 +0100

    remade nuclear to mach the new fuels

--- kubejs/server_scripts/big_mods/gtceu/new_machine_recipes/nuclear.js
+++ kubejs/server_scripts/big_mods/gtceu/new_machine_recipes/nuclear.js
@@ -1,76 +1,73 @@
-
 ServerEvents.recipes(event => {
 
     const greg = event.recipes.gtceu;
 
-    // Array of fuel rods [rod, depleted rod, time in minutes]
-    const rod_use_stuff = [
-        ['gfs:thorium_fuel_rod', 'gfs:depleted_thorium_fuel_rod', 20],
-        ['gfs:low_enriched_uranium_fuel_rod', 'gfs:depleted_low_enriched_uranium_fuel_rod', 10],
-        ['gfs:highly_enriched_uranium_fuel_rod', 'gfs:depleted_highly_enriched_uranium_fuel_rod', 15]
-    ];
 
-// Array of coolants [id, hot coolant, coolant, energy rate]
-    const cooling_stuff = [
-        ['gtceu:distilled_water 4', 'gtceu:nuclear_steam 16', -512, 60 * 20],
-        ['gtceu:sodium_potassium 2', 'gtceu:hot_sodium_potassium 8', -2048, 60 * 10],
-        ['gtceu:pcb_coolant 1', 'gtceu:hot_pcbcoolant 4', -8192, 60 * 5]
-    ];
+    // for nuclear reacting recipes
+    [
+        ['gfs:thorium_fuel', 'gfs:depleted_thorium_fuel', 20],
+        ['gfs:highly_enriched_uranium_fuel', 'gfs:depleted_highly_enriched_uranium_fuel', 15],
+        ['gfs:low_enriched_uranium_fuel', 'gfs:depleted_low_enriched_uranium_fuel', 10],
+        ['gfs:high_radioactive_fuel', 'gfs:depleted_high_radioactive_fuel', 5],
+        ['gfs:low_radioactive_fuel', 'gfs:depleted_low_radioactive_fuel', 3],
+    ].forEach(([fuel, dep, time]) => {
 
-// Array of shapeless recipes [output item, input items]
-    const rod_craft_stuff = [
-        ['gfs:thorium_fuel_rod', ['9x gtceu:thorium_dust']],
-        ['gfs:low_enriched_uranium_fuel_rod', ['gtceu:uranium_235_dust', '8x gtceu:uranium_dust']],
-        ['gfs:highly_enriched_uranium_fuel_rod', ['4x gtceu:uranium_235_dust', '5x gtceu:uranium_dust']]
-    ];
-
-// for nuclear reacting recipes
-    rod_use_stuff.forEach((rod, depRod, timeInMin)=> {
-
-        cooling_stuff.forEach((config, index) => {
-            greg.nuclear_reactor(`${rod}_${index + 1}`)
-                .itemInputs(rod)
-                .itemOutputs(depRod)
+        cooling_stuff.forEach(([liq, hot, eut, dur], index) => {
+            greg.nuclear_reactor(`gfs:${fuel}`)
+                .itemInputs(fuel)
+                .itemOutputs(dep)
                 .perTick(true)
-                .inputFluids(config[0])
-                .outputFluids(config[1])
+                .inputFluids(liq + (index * 2 + 1).toString())
+                .outputFluids(hot + ((index + 1) * 4).toString())
                 .perTick(false)
-                .duration(timeInMin * config[3])
-                .EUt(config[2]);
+                .EUt(-(eut))
+                .duration(time * dur * 20);
         });
     });
 
-// for nuclear turbine recipes
-    cooling_stuff.forEach((hotCoolant, coolant, rate) =>{
-        greg.nuclear_turbine(`${coolant}_${index + 1}`)
-            .inputFluids(hotCoolant)
-            .outputFluids(coolant)
-            .duration(1)
-            .EUt(-rate);
-    });
+    // Array of coolants [id, hot coolant, coolant, energy rate]
+    const cooling_stuff = [
+        // nitrogen maybe ? and make it non reusable ?
+        ['gtceu:pcb_coolant', 'gtceu:hot_pcbcoolant', 8192, 25],
+        ['gtceu:sodium_potassium', 'gtceu:hot_sodium_potassium', 2048, 20],
+        ['gtceu:distilled_water', 'gtceu:nuclear_pure_steam', 512, 10],
+        ['minecraft:water', 'gtceu:nuclear_steam', 256, 5],
+    ];
 
-
-// Add shapeless crafting recipes
-    rod_craft_stuff.forEach(recipe =>
+    // Add shapeless crafting recipes
+    [
+        ['gfs:thorium_fuel', ['9x #forge:dust/thorium']],
+        ['gfs:low_enriched_uranium_fuel', ['#forge:dust/uranium_235', '8x #forge:dust/uraniumt']],
+        ['gfs:highly_enriched_uranium_fuel', ['4x #forge:dust/uranium_235', '5x #forge:dust/uranium']],
+        ['gfs:high_radioactive_fuel', ['6x #forge:dust/uranium']],
+        ['gfs:low_radioactive_fuel', ['3x #forge:dust/uranium']],
+    ].forEach(recipe =>
         event.shapeless(recipe[0], recipe[1])
     );
 
+    // for nuclear turbine recipes
+    cooling_stuff.forEach((liq, hot, eut, dur) => {
+        greg.nuclear_turbine(`gfs:${hot}`)
+            .inputFluids(hot + ((index + 1) * 4).toString())
+            .outputFluids(liq + (index * 2 + 1).toString())
+            .duration(dur * 20)
+            .EUt(eut);
+    });
 
 
-// Array of centrifuge recipes [id, input item, output items, output fluid]
-    const centrifugeRecipes = [
-        ['th_centrifuging', 'gfs:depleted_thorium_fuel_rod', ['3x gtceu:lead_dust', '2x gtceu:bismuth_dust'], 'gtceu:radon 4000'],
-        ['u_centrifuging', 'gfs:depleted_low_enriched_uranium_fuel_rod', ['3x gtceu:plutonium_dust', '2x gtceu:barium_dust'], 'gtceu:krypton 4000'],
-        ['u_plus_centrifuging', 'gfs:depleted_highly_enriched_uranium_fuel_rod', ['5x gtceu:plutonium_dust', '1x gtceu:barium_dust'], 'gtceu:krypton 2000']
-    ];
-
-// Add centrifuge recipes
-    centrifugeRecipes.forEach(([id,inp,out,liq]) => {
+    // doing stuff with the depleted variants
+    [
+        ['th_centrifuge', 'gfs:depleted_thorium_fuel', ['3x gtceu:lead_dust', '2x gtceu:bismuth_dust'], 'gtceu:radon 4000'],
+        ['u_low_centrifuge', 'gfs:depleted_low_enriched_uranium_fuel', ['3x gtceu:plutonium_dust', '2x gtceu:barium_dust'], 'gtceu:krypton 4000'],
+        ['u_high_centrifuge', 'gfs:depleted_highly_enriched_uranium_fuel', ['5x gtceu:plutonium_dust', '1x gtceu:barium_dust'], 'gtceu:krypton 2000'],
+        ['r_low_centrifuge', 'gfs:low_radioactive_fuel', ['3x gtceu:lead_dust'], 'gtceu:krypton 80'],
+        ['r_high_centrifuge', 'gfs:high_radioactive_fuel', ['6x gtceu:lead_dust'], 'gtceu:krypton 200'],
+    ].forEach(([id, inp, out, liq]) => {
         greg.centrifuge(id)
             .itemInputs(inp)
             .itemOutputs(out)
             .outputFluids(liq)
-            .duration(1200)
+            .duration(60 * 20)
             .EUt(420);
     });
 

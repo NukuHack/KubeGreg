d484a7182d8ccc7cf76e69dd213243dfbf51288e
NukuHack <nukuhack@gmail.com>
2024-11-30 11:37:47 +0100

    remove core stuff, also better recipes for higher tier circuits

--- kubejs/server_scripts/big_mods/ae2/cores.js
+++ kubejs/server_scripts/big_mods/ae2/cores.js
@@ -1,6 +1,10 @@
 ServerEvents.recipes(event => {
 
 	const greg = event.recipes.gtceu;
+
+
+	event.remove({ output: "ae2:formation_core" });
+	event.remove({ output: "ae2:annihilation_core" });
 	
 	// ME Cores
 	greg.assembler("gfs:formation_core")
@@ -19,19 +23,19 @@ ServerEvents.recipes(event => {
 		.duration(20*20);
 
 	greg.circuit_assembler("gfs:formation_better")
-		.itemInputs("#gtceu:circuits/ulv","2x #forge:dusts/fluix",
-			"#forge:gems/certus_quartz", "ae2:logic_processor")
-		.inputFluids(Fluid.of("gtceu:red_alloy",288))
-		.itemOutputs("ae2:formation_core")
+		.itemInputs("#gtceu:circuits/lv","4x #forge:dusts/fluix",
+			"2x #forge:gems/certus_quartz", "ae2:logic_processor")
+		.inputFluids(Fluid.of("gtceu:red_alloy",288*2))
+		.itemOutputs("2x ae2:formation_core")
 		.EUt(480)
-		.duration(5*20);
+		.duration(10*20);
 	greg.circuit_assembler("gfs:annihilation_better")
-		.itemInputs("#gtceu:circuits/ulv","2x #forge:dusts/fluix",
-			"#forge:gems/quartz", "ae2:logic_processor")
-		.inputFluids(Fluid.of("gtceu:red_alloy",288))
-		.itemOutputs("ae2:annihilation_core")
+		.itemInputs("#gtceu:circuits/lv","4x #forge:dusts/fluix",
+			"2x #forge:gems/quartz", "ae2:logic_processor")
+		.inputFluids(Fluid.of("gtceu:red_alloy",288*2))
+		.itemOutputs("2x ae2:annihilation_core")
 		.EUt(480)
-		.duration(5*20);
+		.duration(10*20);
 
 
 

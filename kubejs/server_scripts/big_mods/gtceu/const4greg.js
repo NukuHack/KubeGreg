84eb9db20ce3b1dfe6099a2ce07b401238259c92
NukuHack <nukuhack@gmail.com>
2024-12-01 15:55:44 +0100

    comments for help / expanding 'till MAX

--- kubejs/server_scripts/big_mods/gtceu/const4greg.js
+++ kubejs/server_scripts/big_mods/gtceu/const4greg.js
@@ -3,7 +3,8 @@ priority: 100
 
 
 const voltages = [
-    "lv", "mv", "hv", "ev", "iv", "luv", "zpm", "uv",
+    //0     1     2     3     4     5      6      7     8      9      10    11     12     13
+    "lv", "mv", "hv", "ev", "iv", "luv", "zpm", "uv", "uhv", "uev", "uiv", "uxv", "opv", "max"
 ];
 
 // acces just like you would voltage_to_cable["lv"] = "tin"
@@ -16,6 +17,12 @@ const voltage_to_cable = {
     "luv": "niobium_titanium",
     "zpm": "vanadium_gallium",
     "uv": "yttrium_barium_cuprate",
+    //uhv                               // neutronium
+    "uev":"activated_netherite",        // omnium
+    "uiv":"holmium",                    // infinity
+    "uxv":"monium",    // omnium ?
+    //opv
+    "max":"infinity",
 };
 
 
@@ -41,6 +48,12 @@ const voltage_to_eu = {
     "luv": 30720,//luv
     "zpm": 122880,//zpm
     "uv": 491520,//uv
+    "uhv": 1966080,//uhv
+    "uev": 7864320,//uev
+    "uiv": 31457280,//uiv
+    "uxv": 125829120,//uxv
+    "opv": 503316480,//opv
+    "max": 2013265920//max
 };
 
 

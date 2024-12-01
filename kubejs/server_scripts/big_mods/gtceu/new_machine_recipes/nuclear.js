d118002ba427ffbed6e3910ec487fc682deb4701
NukuHack <nukuhack@gmail.com>
2024-12-01 15:54:06 +0100

    nuclear fixes (also changes but don't tell anyone)

--- kubejs/server_scripts/big_mods/gtceu/new_machine_recipes/nuclear.js
+++ kubejs/server_scripts/big_mods/gtceu/new_machine_recipes/nuclear.js
@@ -9,74 +9,75 @@ ServerEvents.recipes(event => {
         ['gtceu:sodium_potassium', 'gtceu:hot_sodium_potassium', 2048, 20],
         ['gtceu:distilled_water', 'gtceu:nuclear_pure_steam', 512, 10],
         ['minecraft:water', 'gtceu:nuclear_steam', 256, 5],
-    ].forEach(([liq, hot, eut, dur],index) => {
-		
-    // for nuclear turbine recipes
-        greg.nuclear_turbine(`gfs:${hot.slice(hot.lastIndesOf(":"))}`)
-            .inputFluids(`${hot} ${((index+1) * 4).toString()}`)
-            .outputFluids(liq + (index * 2 + 1).toString())
-            .duration(dur * 20)
-            .EUt(eut);
-		
-		
-    [
-        ['gfs:thorium_fuel', 'gfs:depleted_thorium_fuel', 20],
-        ['gfs:highly_enriched_uranium_fuel', 'gfs:depleted_highly_enriched_uranium_fuel', 15],
-        ['gfs:low_enriched_uranium_fuel', 'gfs:depleted_low_enriched_uranium_fuel', 10],
-        ['gfs:high_radioactive_fuel', 'gfs:depleted_high_radioactive_fuel', 5],
-        ['gfs:low_radioactive_fuel', 'gfs:depleted_low_radioactive_fuel', 3],
-    ]
-    // for nuclear reacting recipes
-	.forEach(([fuel, dep, time]) => {
-            greg.nuclear_reactor(`${fuel}`)
-                .itemInputs(fuel)
-                .itemOutputs(dep)
-                .perTick(true)
-                .inputFluids(`${liq} ${((index+1)*2).toString()}`)
-                .outputFluids(`${hot} ${((index+1)*4).toString()}`)
-                .perTick(false)
-                .EUt(-(eut))
-                .duration(time * dur * 20);
-    });
-		
+    ].forEach(([liq, hot, eut, dur], index) => {
+        let helpNum = (index + 1) * 2;
+        // for nuclear turbine recipes
+        greg.nuclear_turbine(`gfs:${hot.slice(hot.lastIndexOf(":") + 1)}`)
+            .inputFluids(`${hot} ${helpNum * 2}`)
+            .outputFluids(`${liq} ${helpNum}`)
+            .duration(dur * 10)
+            .EUt(-eut);
+
+
+        [
+            ['gfs:thorium_fuel', 'gfs:depleted_thorium_fuel', 20],
+            ['gfs:highly_enriched_uranium_fuel', 'gfs:depleted_highly_enriched_uranium_fuel', 15],
+            ['gfs:low_enriched_uranium_fuel', 'gfs:depleted_low_enriched_uranium_fuel', 10],
+            ['gfs:high_radioactive_fuel', 'gfs:depleted_high_radioactive_fuel', 5],
+            ['gfs:low_radioactive_fuel', 'gfs:depleted_low_radioactive_fuel', 3],
+        ]
+            // for nuclear reacting recipes
+            .forEach(([fuel, dep, time]) => {
+                greg.nuclear_reactor(`${fuel}/${hot.slice(hot.lastIndexOf(":") + 1)}`)
+                    .itemInputs(fuel)
+                    .itemOutputs(dep)
+                    .perTick(true)
+                    .inputFluids(`${liq} ${helpNum}`)
+                    .outputFluids(`${hot} ${helpNum * 2}`)
+                    .perTick(false)
+                    .EUt(-eut)
+                    .duration(time * dur * 20);
+            });
+
     });
 
-	
-	
 
     // Add shapeless crafting recipes
     [
-        ['gfs:thorium_fuel', ['9x #forge:dust/thorium']],
-        ['gfs:low_enriched_uranium_fuel', ['#forge:dust/uranium_235', '8x #forge:dust/uraniumt']],
-        ['gfs:highly_enriched_uranium_fuel', ['4x #forge:dust/uranium_235', '5x #forge:dust/uranium']],
-        ['gfs:high_radioactive_fuel', ['6x #forge:dust/uranium']],
-        ['gfs:low_radioactive_fuel', ['3x #forge:dust/uranium']],
+        ['thorium_fuel', ['9x #forge:dust/thorium']],
+        ['low_enriched_uranium_fuel', ['#forge:dusts/uranium_235', '8x #forge:dusts/uraniumt']],
+        ['highly_enriched_uranium_fuel', ['4x #forge:dusts/uranium_235', '5x #forge:dusts/uranium']],
+        ['high_radioactive_fuel', ['6x #forge:dusts/uranium']],
+        ['low_radioactive_fuel', ['3x #forge:dusts/uranium']],
     ]
-	.forEach(recipe =>
-        event.shapeless(recipe[0], recipe[1])
-    );
+        .forEach(([name, mats]) =>
+            greg.assembler(`gfs:${name}`)
+                .itemInputs(mats)
+                .itemOutputs(`gfs:${name}`)
+                .duration(10 * 20)
+                .EUt(420)
+        );
 
 
     // doing stuff with the depleted variants
     [
-        ['th_centrifuge', 'gfs:depleted_thorium_fuel', ['3x gtceu:lead_dust', '2x gtceu:bismuth_dust'], 'gtceu:radon 4000'],
-        ['u_low_centrifuge', 'gfs:depleted_low_enriched_uranium_fuel', ['3x gtceu:plutonium_dust', '2x gtceu:barium_dust'], 'gtceu:krypton 4000'],
-        ['u_high_centrifuge', 'gfs:depleted_highly_enriched_uranium_fuel', ['5x gtceu:plutonium_dust', '1x gtceu:barium_dust'], 'gtceu:krypton 2000'],
-        ['r_low_centrifuge', 'gfs:low_radioactive_fuel', ['3x gtceu:lead_dust'], 'gtceu:krypton 80'],
-        ['r_high_centrifuge', 'gfs:high_radioactive_fuel', ['6x gtceu:lead_dust'], 'gtceu:krypton 200'],
+        ['thorium_fuel', ['3x gtceu:lead_dust', '2x gtceu:bismuth_dust'], 'gtceu:radon 4000'],
+        ['low_enriched_uranium_fuel', ['3x gtceu:plutonium_dust', '2x gtceu:barium_dust'], 'gtceu:krypton 4000'],
+        ['highly_enriched_uranium_fuel', ['5x gtceu:plutonium_dust', '1x gtceu:barium_dust'], 'gtceu:krypton 2000'],
+        ['low_radioactive_fuel', ['3x gtceu:lead_dust'], 'gtceu:krypton 80'],
+        ['high_radioactive_fuel', ['6x gtceu:lead_dust'], 'gtceu:krypton 200'],
     ]
-	.forEach(([id, inp, out, liq]) => {
-        greg.centrifuge(`gfs:${id}`)
-            .itemInputs(inp)
-            .itemOutputs(out)
-            .outputFluids(liq)
-            .duration(60 * 20)
-            .EUt(420);
-    });
+        .forEach(([name, out, liq]) => {
+            greg.centrifuge(`gfs:${name}`)
+                .itemInputs(`gfs:depleted_${name}`)
+                .itemOutputs(out)
+                .outputFluids(liq)
+                .duration(60 * 20)
+                .EUt(420);
+        });
 
 
-
-	// recipe for the controllers
+    // recipe for the controllers
 
     event.shaped(Item.of('gtceu:nuclear_reactor'), [
         'EGR',

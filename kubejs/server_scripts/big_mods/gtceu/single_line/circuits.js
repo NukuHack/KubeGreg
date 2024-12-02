b6a937a6ff752aa149e929ea9a43694085410ffe
NukuHack <nukuhack@gmail.com>
2024-12-02 17:00:07 +0100

    just a plan for the greater future

--- kubejs/server_scripts/big_mods/gtceu/single_line/circuits.js
+++ kubejs/server_scripts/big_mods/gtceu/single_line/circuits.js
@@ -8,7 +8,7 @@ ServerEvents.recipes(event => {
         .itemInputs('gtceu:wetware_printed_circuit_board', '4x gtceu:stem_cells', '8x gtceu:polybenzimidazole_small_fluid_pipe', '4x gtceu:neutronium_plate', '16x gtceu:silicone_rubber_foil', '8x gtceu:hsse_bolt')
         .inputFluids('gtceu:sterilized_growth_medium 100')
         .itemOutputs('gtceu:neuro_processing_unit')
-        .duration(5*20)
+        .duration(5 * 20)
         .EUt(voltage_to_eu["zmp"])
         .cleanroom(CleanroomType.CLEANROOM)
 
@@ -21,7 +21,7 @@ ServerEvents.recipes(event => {
         ['inductor', 'gtceu:tritanium_ring', '16x gtceu:fine_holmium_wire']
     ]
 
-    smds.forEach(([item,primary,secondary]) => {
+    smds.forEach(([item, primary, secondary]) => {
         greg.assembler(`complex_smd_${item}`)
             .itemInputs(primary, secondary)
             .itemOutputs(`32x gfs:complex_smd_${item}`)
@@ -58,10 +58,91 @@ ServerEvents.recipes(event => {
         }
     };
 
+
+    const Materials = [
+        ['circuit_board',
+            [
+                `${8 * multi}x #forge:plates/${mat1}`,
+                `${32 * multi}x #forge:foils/${wire1}`,
+                `${32 * multi}x #forge:foils/${mat2}`,
+                `${4 * multi}x gfs:${voltages[tierN - 1]}_processing_unit`,
+                `${1 * multi}xgregtech:circuits/${voltages[tierN - 3]}`,
+                `${8 * multi}x #forge:dusts/${mat2}`
+            ],
+            [mat2, 144],
+        ],
+        ['processing_unit',
+            [
+                `${2 * multi}x gfs:${tier}_circuit_board`,
+                `${2 * multi}x #forge:plates/${mat2}`,
+                `${4 * multi}x #forge:doublewires/${wire2}`,
+                `${2 * small_multi}x ${soc_type}`
+            ],
+            [solder, 576],
+        ],
+        ['processor',
+            [
+                `${1 * multi}x gfs:${tier}_processing_unit`,
+                `${8 * small_multi}x ${smd_type}_capacitor`,
+                `${4 * small_multi}x ${smd_type}_transistor`,
+                `${4 * small_multi}x ${chip_type}`,
+                `${4 * multi}x #forge:wires/${wire1}`
+            ],
+            [solder, 144],
+        ],
+        ['processor_assembly',
+            [
+                `${1 * multi}x gfs:${tier}_processing_unit`,
+                `${4 * multi}x gfs:${tier}_processor`,
+                `${12 * small_multi}x ${chip_type}`,
+                `${8 * small_multi}x ${smd_type}_capacitor`,
+                `${6 * small_multi}x ${smd_type}_transistor`,
+                `${4 * small_multi}x ${smd_type}_resistor`,
+                `${4 * multi}x #forge:doublewires/${wire1}`,
+                `${4 * multi}x #forge:doublewires/${wire2}`
+            ],
+            [solder, 576],
+        ],
+        ['processor_computer',
+            [
+                `${1 * multi}x gfs:${tier}_processing_unit`,
+                `${2 * multi}x gfs:${tier}_processor_assembly`,
+                `${24 * small_multi}x ${chip_type}`,
+                `${12 * small_multi}x ${smd_type}_capacitor`,
+                `${8 * small_multi}x ${smd_type}_transistor`,
+                `${8 * small_multi}x ${smd_type}_diode`,
+                `${8 * small_multi}x ${smd_type}_resistor`,
+                `${8 * small_multi}x ${uhcp_type}`,
+                `${4 * multi}x #forge:doublewires/${wire1}`,
+                `${4 * multi}x #forge:doublewires/${wire2}`,
+                `${8 * multi}x #forge:foils/${rubber}`
+            ],
+            [solder, 576, mat2, 144],
+        ],
+        ['processor_mainframe',
+            [
+                `${4 * multi}x #forge:frames/${mat1}`,
+                `${8 * multi}x gfs:${tier}_processor_computer`,
+                `${24 * small_multi}x ${smd_type}_capacitor`,
+                `${12 * small_multi}x ${smd_type}_transistor`,
+                `${12 * small_multi}x ${smd_type}_diode`,
+                `${12 * small_multi}x ${smd_type}_resistor`,
+                `${12 * small_multi}x ${smd_type}_inductor`,
+                `${32 * small_multi}x ${ram_type}`,
+                `${16 * multi}x #forge:foils/${rubber}`,
+                `${8 * multi}x #forge:doublewires/${wire1}`,
+                `${8 * multi}x #forge:doublewires/${wire2}`,
+                `${8 * multi}x #forge:plates/${mat2}`
+            ],
+            [solder, 1152, mat2, 288],
+        ],
+    ];
+
+
     // Define fluid recipes separately
     const CircuitFluids = {
         matter: {
-            'circuit_board': ['minecraft:water 500'],
+            'circuit_board': ['gtceu:omnium 144'],
             'processing_unit': ['gtceu:soldering_alloy 1152'],
             'processor': ['gtceu:soldering_alloy 144'],
             'processor_assembly': ['gtceu:soldering_alloy 1152'],
@@ -102,13 +183,16 @@ ServerEvents.recipes(event => {
         'processor_computer',
         'processor_mainframe',
     ];
-    let procNums = [16, 8, 8, 4, 1, 1,];
+    let procNums = [4, 8, 8, 4, 1, 1,];
     const procDur = [30, 5, 7, 10, 15, 30];
 
-    craftingDetails.forEach(([type,tier], index_stuff) => {
+    craftingDetails.forEach(([type, tier], index_stuff) => {
         procTiers.forEach((name, index) => {
             let num = procNums[index];
-            let [currMat,currFlu] = [CircuitMaterials[type],CircuitFluids[type]];
+            let [currMat, currFlu] = [CircuitMaterials[type], CircuitFluids[type]];
+            let [[mat1, mat2, wire2], wire1] = [volt_to_material[tier], voltage_to_cable[tier]];
+            let [solder, rubber] = [volt_to_extra[tier]];
+            let [smd_type, ram_type, soc_type, chip_type, multi, small_multi] = MatTypesHelp[tier];
             // decided to go with uv for everything (for now atleast)
             let [res_cwu, res_dur, res_eut] = tier_to_research["uv"];
             if (type == 'circuit_board') {
@@ -116,15 +200,15 @@ ServerEvents.recipes(event => {
                     .itemInputs(currMat[name])
                     .inputFluids(currFlu[name])
                     .itemOutputs(`${num}x gfs:${procTiers[index_stuff][0]}_${name}`)
-                    .duration(procDur[index]*20)
+                    .duration(procDur[index] * 20)
                     .EUt(voltage_to_eu[tier]);
             } else {
-                let help = (type=="matter"?`gtceu:neuro_${name}`:`gfs:${craftingDetails[index_stuff-1][0]}_${name}`)
+                let help = (type == "matter" ? `gtceu:neuro_${name}` : `gfs:${craftingDetails[index_stuff - 1][0]}_${name}`)
                 greg.assembly_line(`gfs:${procTiers[index_stuff][0]}_${name}`)
                     .itemInputs(currMat[name])
                     .inputFluids(currFlu[name])
                     .itemOutputs(`${num}x gfs:${procTiers[index_stuff][0]}_${name}`)
-                    .duration(procDur[index]*20)
+                    .duration(procDur[index] * 20)
                     .EUt(voltage_to_eu[voltage_lesser[tier]])
                     .stationResearch(b => b
                         .researchStack(help)
@@ -134,7 +218,6 @@ ServerEvents.recipes(event => {
             }
         });
     });
-
 
 
     /*

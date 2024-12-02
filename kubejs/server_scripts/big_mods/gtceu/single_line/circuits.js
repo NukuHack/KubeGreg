c9126e62b0eaf250adc243e5ab852c3bb91bba48
NukuHack <nukuhack@gmail.com>
2024-12-03 00:23:43 +0100

    remade the circuit making in (another) different way ...

--- kubejs/server_scripts/big_mods/gtceu/single_line/circuits.js
+++ kubejs/server_scripts/big_mods/gtceu/single_line/circuits.js
@@ -30,84 +30,84 @@ ServerEvents.recipes(event => {
 
 
     let Materials = [
-        () => ['circuit_board',
+        (tier, [mat1, mat2, wire2], wire1, [multi, small_multi, smd_type, ram_type, soc_type, chip_type, uhcp_type], [solder, plastic, rubber], tierN) => ['circuit_board',
             [
-                `${8 * multi}x #forge:plates/${mat1}`,
-                `${32 * multi}x #forge:foils/${wire1}`,
-                `${32 * multi}x #forge:foils/${mat2}`,
-                `${4 * multi}x gfs:${voltages[tierN - 1]}_processing_unit`,
+                `${4 * multi}x #forge:plates/${mat1}`,
+                `${16 * multi}x #forge:foils/${wire1}`,
+                `${16 * multi}x #forge:foils/${mat2}`,
+                `${2 * multi}x ${tierN == 8 ? `gtceu:neuro` : `gfs:${voltages[tierN - 1]}`}_processing_unit`,
                 `${1 * multi}x gregtech:circuits/${voltages[tierN - 3]}`,
-                `${8 * multi}x #forge:dusts/${mat2}`
+                `${4 * multi}x #forge:dusts/${mat2}`
             ],
             [`${mat2} ${144 * multi / 2}`],
             [4, 30],
         ],
-        () => ['processing_unit',
+        (tier, [mat1, mat2, wire2], wire1, [multi, small_multi, smd_type, ram_type, soc_type, chip_type, uhcp_type], [solder, plastic, rubber], tierN) => ['processing_unit',
             [
-                `${2 * multi}x gfs:${tier}_circuit_board`,
-                `${2 * multi}x #forge:plates/${mat2}`,
-                `${4 * multi}x gtceu:${wire2}_double_wire`,
-                `${2 * small_multi}x ${soc_type}`
+                `${1 * multi}x gfs:${tier}_circuit_board`,
+                `${1 * multi}x #forge:plates/${mat2}`,
+                `${2 * multi}x gtceu:${wire2}_double_wire`,
+                `${1 * small_multi}x ${soc_type}`
             ],
             [`${solder} ${576 * multi / 2}`],
             [4, 5],
         ],
-        () => ['processor',
+        (tier, [mat1, mat2, wire2], wire1, [multi, small_multi, smd_type, ram_type, soc_type, chip_type, uhcp_type], [solder, plastic, rubber], tierN) => ['processor',
             [
                 `${1 * multi}x gfs:${tier}_processing_unit`,
-                `${8 * small_multi}x ${smd_type}_capacitor`,
-                `${4 * small_multi}x ${smd_type}_transistor`,
-                `${4 * small_multi}x ${chip_type}`,
-                `${4 * multi}x gtceu:${wire1}_single_wire`
+                `${4 * small_multi}x ${smd_type}_capacitor`,
+                `${2 * small_multi}x ${smd_type}_transistor`,
+                `${2 * small_multi}x ${chip_type}`,
+                `${2 * multi}x gtceu:${wire1}_single_wire`
             ],
             [`${solder} ${144 * multi / 2}`],
             [4, 7],
         ],
-        () => ['processor_assembly',
+        (tier, [mat1, mat2, wire2], wire1, [multi, small_multi, smd_type, ram_type, soc_type, chip_type, uhcp_type], [solder, plastic, rubber], tierN) => ['processor_assembly',
             [
                 `${1 * multi}x gfs:${tier}_processing_unit`,
-                `${4 * multi}x gfs:${tier}_processor`,
-                `${12 * small_multi}x ${chip_type}`,
-                `${8 * small_multi}x ${smd_type}_capacitor`,
-                `${6 * small_multi}x ${smd_type}_transistor`,
-                `${4 * small_multi}x ${smd_type}_resistor`,
-                `${4 * multi}x gtceu:${wire1}_double_wire`,
-                `${4 * multi}x gtceu:${wire2}_double_wire`
+                `${2 * multi}x gfs:${tier}_processor`,
+                `${6 * small_multi}x ${chip_type}`,
+                `${4 * small_multi}x ${smd_type}_capacitor`,
+                `${3 * small_multi}x ${smd_type}_transistor`,
+                `${2 * small_multi}x ${smd_type}_resistor`,
+                `${2 * multi}x gtceu:${wire1}_double_wire`,
+                `${2 * multi}x gtceu:${wire2}_double_wire`
             ],
             [`${solder} ${576 * multi / 2}`],
             [4, 10],
         ],
-        () => ['processor_computer',
+        (tier, [mat1, mat2, wire2], wire1, [multi, small_multi, smd_type, ram_type, soc_type, chip_type, uhcp_type], [solder, plastic, rubber], tierN) => ['processor_computer',
             [
                 `${1 * multi}x gfs:${tier}_processing_unit`,
-                `${2 * multi}x gfs:${tier}_processor_assembly`,
-                `${24 * small_multi}x ${chip_type}`,
-                `${12 * small_multi}x ${smd_type}_capacitor`,
-                `${8 * small_multi}x ${smd_type}_transistor`,
-                `${8 * small_multi}x ${smd_type}_diode`,
-                `${8 * small_multi}x ${smd_type}_resistor`,
-                `${8 * small_multi}x ${uhcp_type}`,
-                `${4 * multi}x gtceu:${wire1}_double_wire`,
-                `${4 * multi}x gtceu:${wire2}_double_wire`,
-                `${8 * multi}x #forge:foils/${rubber}`
+                `${1 * multi}x gfs:${tier}_processor_assembly`,
+                `${12 * small_multi}x ${chip_type}`,
+                `${6 * small_multi}x ${smd_type}_capacitor`,
+                `${4 * small_multi}x ${smd_type}_transistor`,
+                `${4 * small_multi}x ${smd_type}_diode`,
+                `${4 * small_multi}x ${smd_type}_resistor`,
+                `${4 * small_multi}x ${uhcp_type}`,
+                `${2 * multi}x gtceu:${wire1}_double_wire`,
+                `${2 * multi}x gtceu:${wire2}_double_wire`,
+                `${4 * multi}x #forge:foils/${plastic}`,
             ],
             [`${solder} ${576 * multi / 2}`, `${mat2} ${144 * multi / 2}`],
             [1, 15],
         ],
-        () => ['processor_mainframe',
+        (tier, [mat1, mat2, wire2], wire1, [multi, small_multi, smd_type, ram_type, soc_type, chip_type, uhcp_type], [solder, plastic, rubber], tierN) => ['processor_mainframe',
             [
-                `${4 * multi}x #forge:frames/${mat1}`,
-                `${8 * multi}x gfs:${tier}_processor_computer`,
-                `${24 * small_multi}x ${smd_type}_capacitor`,
-                `${12 * small_multi}x ${smd_type}_transistor`,
-                `${12 * small_multi}x ${smd_type}_diode`,
-                `${12 * small_multi}x ${smd_type}_resistor`,
-                `${12 * small_multi}x ${smd_type}_inductor`,
-                `${32 * small_multi}x ${ram_type}`,
-                `${16 * multi}x #forge:foils/${rubber}`,
-                `${8 * multi}x gtceu:${wire1}_double_wire`,
-                `${8 * multi}x gtceu:${wire2}_double_wire`,
-                `${8 * multi}x #forge:plates/${mat2}`
+                `${2 * multi}x #forge:frames/${mat1}`,
+                `${4 * multi}x gfs:${tier}_processor_computer`,
+                `${4 * multi}x #forge:plates/${mat2}`
+                    `${12 * small_multi}x ${smd_type}_capacitor`,
+                `${6 * small_multi}x ${smd_type}_transistor`,
+                `${6 * small_multi}x ${smd_type}_diode`,
+                `${6 * small_multi}x ${smd_type}_resistor`,
+                `${6 * small_multi}x ${smd_type}_inductor`,
+                `${16 * small_multi}x ${ram_type}`,
+                `${4 * multi}x gtceu:${wire1}_double_wire`,
+                `${4 * multi}x gtceu:${wire2}_double_wire`,
+                `${8 * multi}x #forge:foils/${plastic}`,
             ],
             [`${solder} ${1152 * multi / 2}`, `${mat2} ${288 * multi / 2}`],
             [1, 30],
@@ -126,13 +126,15 @@ ServerEvents.recipes(event => {
         Materials.forEach((MaterialHelper, index) => {
 
             let tier = voltages[index + 8];
-            let tierN = voltages.indexOf(tier);
-            let [[mat1, mat2, wire2], wire1] = [volt_to_material[tier], voltage_to_cable[tier]];
-            let [solder, rubber] = [volt_to_extra[tier]];
-            let [smd_type, multi, ram_type, small_multi, soc_type, chip_type, uhcp_type] = MatTypesHelp[tier];
             // maybe I will just make a function for every type of circuit stuff, so it will just calculate it that time when it's called ....
             // I just hate function calls inside recipe creations
-            let [name, inp, flui, [out_multi, dur]] = MaterialHelper();
+            let [name, inp, flui, [out_multi, dur]] = MaterialHelper(
+                tier,
+                volt_to_material[tier], voltage_to_cable[tier],
+                MatTypesHelp[tier],
+                volt_to_extra[tier],
+                voltages.indexOf(tier)
+            );
 
             if (name == 'circuit_board') {
                 // something like : monic_circuit_board

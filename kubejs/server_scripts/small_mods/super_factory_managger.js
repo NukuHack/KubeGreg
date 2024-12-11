5069bc529ab268404245a00eecba4de4aaa5e2e8
NukuHack <nukuhack@gmail.com>
2024-12-11 18:08:00 +0100

    spacing fix (for sfm)

--- kubejs/server_scripts/small_mods/super_factory_managger.js
+++ kubejs/server_scripts/small_mods/super_factory_managger.js
@@ -12,50 +12,48 @@ ServerEvents.recipes(event => {
 
     greg
         .assembler("gfs:sfm_manager")
-.circuit(12)
-.itemInputs( 
-    '2x #gtceu:circuits/ev',
-    '3x gtceu:cobalt_brass_large_item_pipe',
-    '4x gtceu:ev_robot_arm',
-    'gtceu:computer_monitor_cover',
-    '8x gtceu:double_stainless_steel_plate',
-    '2x gtceu:item_filter',
-    'gtceu:data_access_hatch'
-)
-.itemOutputs('sfm:manager')
-.duration(1200)
-.EUt(1760);
+        .circuit(12)
+        .itemInputs(
+            '2x #gtceu:circuits/ev',
+            '3x gtceu:cobalt_brass_large_item_pipe',
+            '4x gtceu:ev_robot_arm',
+            'gtceu:computer_monitor_cover',
+            '8x gtceu:double_stainless_steel_plate',
+            '2x gtceu:item_filter',
+            'gtceu:data_access_hatch'
+        )
+        .itemOutputs('sfm:manager')
+        .duration(1200)
+        .EUt(1760);
 
     // super_factory_cable
     greg
         .assembler("gfs:sfm_cable")
-.itemInputs("8x #forge:plates/titanium", "2x #gtceu:circuits/iv",
-            "4x #forge:plates/ender_eye", 
-    '8x gtceu:long_distance_item_pipeline',
-    '8x gtceu:long_distance_fluid_pipeline',
-    '2x gtceu:ev_electric_motor',
-    '18x gtceu:black_steel_plate'
-)
-.itemOutputs('8x sfm:cable')
-.duration(870)
-.EUt(980)
+        .itemInputs("8x #forge:plates/titanium", "2x #gtceu:circuits/iv",
+            "4x #forge:plates/ender_eye",
+            '8x gtceu:long_distance_item_pipeline',
+            '8x gtceu:long_distance_fluid_pipeline',
+            '2x gtceu:ev_electric_motor',
+            '18x gtceu:black_steel_plate'
+        )
+        .itemOutputs('8x sfm:cable')
+        .duration(870)
+        .EUt(980)
 
 
     greg.circuit_assembler('gfs:sfm_disk')
-.itemInputs(
-    '16x gtceu:fine_electrum_wire',
-    'gtceu:qbit_cpu_chip',
-    '2x gtceu:nano_cpu_chip',
-    '8x gtceu:ram_chip',
-    '4x gtceu:polyethylene_plate'
-)
-.notConsumable('1x gtceu:data_stick')
-.inputFluids('gtceu:soldering_alloy 144')
-.duration(600)
-.EUt(1920)
-.itemOutputs('4x sfm:disk')
-
-
+        .itemInputs(
+            '16x gtceu:fine_electrum_wire',
+            'gtceu:qbit_cpu_chip',
+            '2x gtceu:nano_cpu_chip',
+            '8x gtceu:ram_chip',
+            '4x gtceu:polyethylene_plate'
+        )
+        .notConsumable('1x gtceu:data_stick')
+        .inputFluids('gtceu:soldering_alloy 144')
+        .duration(600)
+        .EUt(1920)
+        .itemOutputs('4x sfm:disk')
 
 
 });
